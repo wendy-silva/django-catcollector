b671d3e2e99866c8bd88ae8dd80103f7bc0f7b7b
wendy-silva <silvawendy0131@gmail.com>
2024-09-12 10:00:22 -0400

    django-rep

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 # add Feeding to the import
-from .models import Cat, Feeding
+from .models import Cat, Feeding, Toy
 
 admin.site.register(Cat)
 # register the new Feeding Model 
 admin.site.register(Feeding)
+
+admin.site.register(Toy)
--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, CatList, CatDetail, FeedingListCreate, FeedingDetail # additional imports
+from .views import Home, CatList, CatDetail, FeedingListCreate, FeedingDetail, ToyList, ToyDetail # additional imports
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
@@ -8,4 +8,6 @@ urlpatterns = [
   path('cats/<int:id>/', CatDetail.as_view(), name='cat-detail'),
   path('cats/<int:cat_id>/feedings/', FeedingListCreate.as_view(), name='feeding-list-create'),
 	path('cats/<int:cat_id>/feedings/<int:id>/', FeedingDetail.as_view(), name='feeding-detail'),
+  path('toys/', ToyList.as_view(), name='toy-list'),
+  path('toys/<int:id>/', ToyDetail.as_view(), name='toy-detail'),
 ]
